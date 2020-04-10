9ce58f1de3934f0e4f00ff33985bab5a80e05a18
Dan Feeney <daniel.feeney@colorado.edu>
2020-04-10 13:04:41 -0600

    Fixing desiredStepLength bug

--- analyzeLoadsol.m
+++ analyzeLoadsol.m
@@ -2,9 +2,8 @@
 clear
 addpath('C:\Users\Daniel.Feeney\Documents\novel_data')
 % The files should be named sub_balance_Config_trialNo - Forces.txt
-input_dir = 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Endurance Protocol Trail Run\Outdoor_Protocol_March2020\KH';% Change to correct filepath
-%input_dir = 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Hike Work Research\OutdoorProtocolMarch2020';% Change to correct filepath
-
+%input_dir = 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Endurance Protocol Trail Run\Outdoor_Protocol_March2020\MM';% Change to correct filepath
+input_dir = 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Hike Work Research\OutdoorProtocolMarch2020';% Change to correct filepath
 
 cd(input_dir)
 files = dir('*.txt');
@@ -13,14 +12,14 @@ dataList = sort(dataList);
 [f,~] = listdlg('PromptString','Select data files for all subjects in group','SelectionMode','multiple','ListString',dataList);
 NumbFiles = length(f);
 
-outputAllConfigs = {'Config','TrialNo','Side','PrePost','stanceTime','PkTotal', 'ImpulseTotal', 'PkHeel', 'HeelImpulse',...
+outputAllConfigs = {'Subject','Config','TrialNo','Side','PrePost','stanceTime','PkTotal', 'ImpulseTotal', 'PkHeel', 'HeelImpulse',...
     'PkLat','LatImpulse','PkMed','MedImpulse','RateTotal'};
 
 %Define constants and options
 fThresh = 50; %below this value will be set to 0.
 minStepLen = 10; %minimal step length
 writeData = 1; %will write to spreadsheet if 1 entered
-desiredStepLength = 20; %length to look forward after initial contact
+desiredStepLength = 75; %length to look forward after initial contact
 apple = 0; %1 for apple 0 otherwise
 
 for s = 1:NumbFiles
@@ -41,7 +40,9 @@ for s = 1:NumbFiles
 
     dat = dat(:,1:10);
     
-    splitFName = strsplit(fileName,'_'); configName = splitFName{2};
+    splitFName = strsplit(fileName,'_'); 
+    subName = splitFName{1};
+    configName = splitFName{2};
     trialNo = splitFName{4}(1); PrePost = splitFName{3};
     
     %% left side
@@ -87,10 +88,10 @@ for s = 1:NumbFiles
     stepsToRem = [1,2,3,vecLen - 2, vecLen - 1];
     lic(stepsToRem) = []; lto(stepsToRem) = []; LStepLens(stepsToRem) = [];
     
-    LTot = zeros(length(lic),21);
-    LHeel = zeros(length(lic),21);
-    LLat = zeros(length(lic),21);
-    LMed = zeros(length(lic),21);
+    LTot = zeros(length(lic),desiredStepLength+1);
+    LHeel = zeros(length(lic),desiredStepLength+1);
+    LLat = zeros(length(lic),desiredStepLength+1);
+    LMed = zeros(length(lic),desiredStepLength+1);
     
     for i = 1:(length(lic) - 1)
         LTot(i,:) = LForce(lic(i):lic(i)+desiredStepLength);
@@ -144,10 +145,10 @@ for s = 1:NumbFiles
     stepsToRem = [1,2,3,vecLen - 2, vecLen - 1];
     ric(stepsToRem) = []; rto(stepsToRem) = []; RStepLens(stepsToRem) = [];
     
-    RTot = zeros(length(ric),21);
-    RHeel = zeros(length(ric),21);
-    RLat = zeros(length(ric),21);
-    RMed = zeros(length(ric),21);
+    RTot = zeros(length(ric),desiredStepLength+1);
+    RHeel = zeros(length(ric),desiredStepLength+1);
+    RLat = zeros(length(ric),desiredStepLength+1);
+    RMed = zeros(length(ric),desiredStepLength+1);
     
     for i = 1:(length(ric) - 1)
         RTot(i,:) = RForce(ric(i):ric(i)+desiredStepLength);
@@ -218,8 +219,10 @@ for s = 1:NumbFiles
     LRtmp = vertcat(lVecTmp, rVecTmp); %left or right side into a vector
     configNameTmp = cell(length(LRtmp),1); %configuration name from early in the file
     trialNoTmp = cell(length(LRtmp),1); %configuration name from early in the file
-    PrePostTmp = cell(length(LRtmp),1); %configuration name from early in the file
+    PrePostTmp = cell(length(LRtmp),1); %condition from early in the file
+    subNameTmp = cell(length(LRtmp),1); %subject name from earlier in the file
     for i = 1:length(LRtmp)
+        subNameTmp(i) = {subName};
         configNameTmp(i) = {configName};
         trialNoTmp(i) = {trialNo};
         PrePostTmp(i) = {PrePost};
@@ -228,7 +231,7 @@ for s = 1:NumbFiles
     % clean up and concatenate
     kinData = [leftFeat; rightFeat];
     kinData = num2cell(kinData);
-    kinData = horzcat(configNameTmp,trialNoTmp,LRtmp, PrePostTmp, kinData); %modulate this to subject name and config
+    kinData = horzcat(subNameTmp,configNameTmp,trialNoTmp,LRtmp, PrePostTmp, kinData); %modulate this to subject name and config
     outputAllConfigs = vertcat(outputAllConfigs, kinData);
     
     clearvars LRtmp configNameTmp rightFeat leftFeat ric lic rto lto
@@ -239,30 +242,35 @@ if writeData == 1
     % Convert cell to a table and use first row as variable names
     T = cell2table(outputAllConfigs(2:end,:),'VariableNames',outputAllConfigs(1,:));
     % Write the table to a CSV file
-    writetable(T,'KHNew.csv')
+    writetable(T,'SH.csv')
 end
 % 
 %% make plots 
-% figure(3)
+% 
+% subplot(2,2,1)
 % shadedErrorBar(0:desiredStepLength, LTot, {@mean, @std}, 'lineprops','blue');
 % hold on
 % shadedErrorBar(0:desiredStepLength, RTot, {@mean, @std}, 'lineprops','red');
 % title('Total Force')
+% ylim([0 1500])
 % 
-% figure(5)
+% subplot(2,2,2)
 % shadedErrorBar(0:desiredStepLength, LHeel, {@mean, @std},'lineprops', 'blue')
 % hold on
 % shadedErrorBar(0:desiredStepLength, RHeel, {@mean, @std},'lineprops', 'red')
 % title('left (blue) vs. right (red) heel')
+% ylim([0 1000])
 % 
-% figure(6)
+% subplot(2,2,3)
 % shadedErrorBar(0:desiredStepLength, LMed, {@mean, @std},'lineprops', 'blue')
 % hold on
 % shadedErrorBar(0:desiredStepLength, RMed, {@mean, @std},'lineprops', 'red')
 % title('left (blue) vs. right (red) medial forefoot')
+% ylim([0 1000])
 % 
-% figure(7)
+% subplot(2,2,4)
 % shadedErrorBar(0:desiredStepLength, LLat, {@mean, @std},'lineprops', 'blue')
 % hold on
 % shadedErrorBar(0:desiredStepLength, RLat, {@mean, @std},'lineprops', 'red')
 % title('left (blue) vs. right (red) lateral forefoot')
+% ylim([0 1000])
