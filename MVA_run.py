301e750ad3ef2ed790b3dca936ee186ca9aa7190
Dan Feeney <daniel.feeney@colorado.edu>
2020-09-03 10:23:11 -0600

    Looping through files

--- MVA_run.py
+++ MVA_run.py
@@ -11,24 +11,7 @@ import matplotlib.pyplot as plt
 import os
 from scipy.misc import electrocardiogram
 from scipy.signal import find_peaks
-
-# Read in files
-# only read .asc files for this work
-fPath = 'C:/Users/Daniel.Feeney/Dropbox (Boa)/EndurancePerformance/SalomonQuicklace_Aug2020/Joe_Pressure_Data/'
-fileExt = r".csv"
-entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
-fThresh = 200; #below this value will be set to 0.
-
-# hard code a single entry for now
-BOAfile = entries[0]
-boa = pd.read_csv(fPath+BOAfile, header = 0)
-boa.columns = ['Time', 'ToeForce', 'ToeMaxP', 'ToeMeanP', 'Toe%mean', 'LatFFForce','LatFMaxP','LatFMeanP','LatF%mean',
-               'HeelForce','HeelMaxP','HeelMeanP','Heel%mean','MedFForce','MedFMaxP','MedFMeanP','MedF%mean',
-               'LatMFForce','LatMFMaxP', 'LatMFMeanP', 'LatMF%mean', 'MedMFForce', 'MedMFMaxP', 'MedMFMeanP', 'MedMF%mean']
-boa['totalForce'] = boa['ToeForce'] + boa['LatFFForce'] + boa['HeelForce'] + boa['MedFForce'] + boa['LatMFForce'] + boa['MedMFForce']
-
-force = np.array(boa['totalForce'])
-force[force<fThresh] = 0
+import seaborn as sns
 
 # list of functions 
 # finding landings on the force plate once the filtered force exceeds the force threshold
@@ -57,10 +40,94 @@ def trimTakeoffs(landings, takeoffs):
     if len(takeoffs) < len(landings):
         del(landings[-1])
     return(landings)
+# Read in files
+# only read .asc files for this work
+fPath = 'C:/Users/Daniel.Feeney/Dropbox (Boa)/EndurancePerformance/SalomonQuicklace_Aug2020/Joe_Pressure_Data/'
+fileExt = r".csv"
+entries = [fName for fName in os.listdir(fPath) if fName.endswith(fileExt)]
+fThresh = 200; #below this value will be set to 0.
+
+# hard code a single entry for now
+#BOAfile = entries[0]
+
+## Preallocate lists
+fileName = []
+peakToeP = []
+meanToeP = []
+peakLFFP = []
+meanLFFP = []
+peakMFFP = []
+meanMFFP = []
+peakLMFP = []
+meanLMFP = []
+peakMMFP = []
+meanMMFP = []
 
 
-ric_initial = findLandings(force)
-rto_initial = findTakeoffs(force)
+for file in entries:
+    try:
+        # Read in a single file as pandas DF, calc total force
+        fName = file #Load one file at a time
+        boa = pd.read_csv(fPath+fName, header = 0)
+        boa.columns = ['Time', 'ToeForce', 'ToeMaxP', 'ToeMeanP', 'Toe%mean', 'LatFFForce','LatFMaxP','LatFMeanP','LatF%mean',
+               'HeelForce','HeelMaxP','HeelMeanP','Heel%mean','MedFForce','MedFMaxP','MedFMeanP','MedF%mean',
+               'LatMFForce','LatMFMaxP', 'LatMFMeanP', 'LatMF%mean', 'MedMFForce', 'MedMFMaxP', 'MedMFMeanP', 'MedMF%mean']
+        boa['totalForce'] = boa['ToeForce'] + boa['LatFFForce'] + boa['HeelForce'] + boa['MedFForce'] + boa['LatMFForce'] + boa['MedMFForce']
 
-ric = trimLandings(ric_initial, rto_initial)
-rto = trimTakeoffs(ric, rto_initial) #Error here
+## For automation, cutoff the force below the threshold to define approximate landings
+        force = np.array(boa['totalForce'])
+        force[force<fThresh] = 0
+        ric = findLandings(force)
+        
+        # Loop through the landings
+        for landing in ric:
+            try:
+                fileName.append(fName)
+                peakToeP.append(np.max(boa['ToeMaxP'][landing:landing+20]))
+                meanToeP.append(np.max(boa['ToeMeanP'][landing:landing+20])/np.max(boa['ToeMaxP'][landing:landing+20]))
+                peakLFFP.append(np.max(boa['LatFMaxP'][landing:landing+20]))
+                meanLFFP.append(np.max(boa['LatFMeanP'][landing:landing+20])/np.max(boa['LatFMaxP'][landing:landing+20]))
+                peakMFFP.append(np.max(boa['MedFMaxP'][landing:landing+20]))
+                meanMFFP.append(np.max(boa['MedFMeanP'][landing:landing+20])/np.max(boa['MedFMaxP'][landing:landing+20]))
+                peakLMFP.append(np.max(boa['LatMFMaxP'][landing:landing+20]))
+                meanLMFP.append(np.max(boa['LatMFMeanP'][landing:landing+20])/np.max(boa['LatMFMaxP'][landing:landing+20]))  
+                peakMMFP.append(np.max(boa['MedMFMaxP'][landing:landing+20]))
+                meanMMFP.append(np.max(boa['MedMFMeanP'][landing:landing+20])/np.max(boa['MedMFMaxP'][landing:landing+20]))  
+            except:
+                print(landing)
+                
+    except:
+        print(file)
+ 
+       
+outcomes = pd.DataFrame({'fName':list(fileName),'ToeMaxP':list(peakToeP), 'meanToeP': list(meanToeP), 'PeakLFFP': list(peakLFFP),
+                 'meanLFFP': list(meanLFFP), 'peakMFFP': list(peakMFFP),'meanMFFP': list(meanMFFP), 'peakLMFP':list(peakLMFP),
+                 'meanLMFP':list(meanLMFP), 'peakMMFP':list(peakMMFP), 'meanMMFP':list(meanMMFP)})
+
+filt_outcomes = outcomes.loc[outcomes['ToeMaxP'] > 100]
+    
+ax = sns.boxplot(y='ToeMaxP', x='fName', 
+                 data=filt_outcomes, 
+                 palette="colorblind")
+ax.set(xlabel='Condition', ylabel='Max Toe Pressure')
+
+ax2 = sns.boxplot(y='meanToeP', x='fName', 
+                 data=filt_outcomes, 
+                 palette="colorblind")
+ax2.set(xlabel='Condition', ylabel='Mean/Max Toe Pressure')
+
+sns.boxplot(y='PeakLFFP', x='fName', 
+                 data=filt_outcomes, 
+                 palette="colorblind")
+
+sns.boxplot(y='peakMFFP', x='fName', 
+                 data=filt_outcomes, 
+                 palette="colorblind")
+
+sns.boxplot(y='peakLMFP', x='fName', 
+                 data=filt_outcomes, 
+                 palette="colorblind")
+
+sns.boxplot(y='peakMMFP', x='fName', 
+                 data=filt_outcomes, 
+                 palette="colorblind")
